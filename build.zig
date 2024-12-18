31fb3a1d5a522feb93055154af0ecd1a04e8a5f4
harkamal <gajinder@zeam.in>
2024-12-18 20:13:55 +0530

    rename modules

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("snappy.zig", Builder.Module.CreateOptions{
+    const mod = b.addModule("snappyz", Builder.Module.CreateOptions{
         .root_source_file = b.path("snappy.zig"),
         .target = target,
         .optimize = optimize,
@@ -12,7 +12,7 @@ pub fn build(b: *Builder) void {
     _ = mod;
 
     const lib = b.addStaticLibrary(.{
-        .name = "snappy",
+        .name = "snappyz",
         .root_source_file = .{ .cwd_relative = "snappy.zig" },
         .optimize = optimize,
         .target = target,
